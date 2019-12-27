2a74b34436bbc36faf46e986b9eacaab2ce62667
Vasyl Osypchuk <leveldorado@gmail.com>
2019-12-28 00:30:18 +0200

    log server stop

--- backend/cmd/battleship/main.go
+++ backend/cmd/battleship/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"log"
 	"time"
+
+	"github.com/tylerb/graceful"
 
 	"github.com/leveldorado/battleship/backend/pkg/http/rest"
 	"github.com/leveldorado/battleship/backend/pkg/starting"
-	"github.com/tylerb/graceful"
 )
 
 func main() {
 	g := starting.NewService()
 	s := rest.CreateServer(g)
-	s.Start(":80")
-	graceful.ListenAndServe(s.Server, 5*time.Second)
+	s.Server.Addr = ":80"
+	err := graceful.ListenAndServe(s.Server, 5*time.Second)
+	log.Println("server stopped with error:", err)
 }
